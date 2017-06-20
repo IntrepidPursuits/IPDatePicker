6af3700a387f4c088cf656188dc758c9431ba036
Andrew Dolce <andrew@intrepid.io>
2017-06-19 21:17:53 -0400

    Expand example by changing the color of the AM/PM component

--- Example/IPDatePicker/ViewController.swift
+++ Example/IPDatePicker/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import IPDatePicker
 import PureLayout
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, IPDatePickerDelegate {
     let picker = IPDatePicker()
 
     override func viewDidLoad() {
@@ -18,5 +18,20 @@ class ViewController: UIViewController {
 
         view.addSubview(picker.view)
         picker.view.autoCenterInSuperview()
+
+        picker.delegate = self
+    }
+
+    // MARK: - IPDatePickerDelegate
+
+    func datePicker(_ datePicker: IPDatePicker, attributedSymbolForComponent component: IPDatePickerComponent, row: Int, suggestedSymbol: String) -> NSAttributedString? {
+        guard component == .amPm else {
+            return nil
+        }
+
+        let attributes = [
+            NSForegroundColorAttributeName: UIColor.blue
+        ]
+        return NSAttributedString(string: suggestedSymbol, attributes: attributes)
     }
 }
