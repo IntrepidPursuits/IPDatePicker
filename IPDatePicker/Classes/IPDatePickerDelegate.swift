df5a5eca96508be2bbd83fd3da6b8289bb9c6dbb
Andrew Dolce <andrew@intrepid.io>
2017-06-19 21:17:32 -0400

    Fix access level of delegate extension

--- IPDatePicker/Classes/IPDatePickerDelegate.swift
+++ IPDatePicker/Classes/IPDatePickerDelegate.swift
@@ -9,35 +9,35 @@
 import UIKit
 
 public protocol IPDatePickerDelegate: class {
-    func datePicker(_ datePicker: IPDatePicker, viewForItemForComponent: IPDatePickerComponent, row: Int, suggestedSymbol: String, reusing view: UIView?) -> UIView?
-    func datePicker(_ datePicker: IPDatePicker, attributedSymbolForComponent: IPDatePickerComponent, row: Int, suggestedSymbol: String) -> NSAttributedString?
-    func datePicker(_ datePicker: IPDatePicker, symbolForComponent: IPDatePickerComponent, row: Int, suggestedSymbol: String) -> String?
+    func datePicker(_ datePicker: IPDatePicker, viewForItemForComponent component: IPDatePickerComponent, row: Int, suggestedSymbol: String, reusing view: UIView?) -> UIView?
+    func datePicker(_ datePicker: IPDatePicker, attributedSymbolForComponent component: IPDatePickerComponent, row: Int, suggestedSymbol: String) -> NSAttributedString?
+    func datePicker(_ datePicker: IPDatePicker, symbolForComponent component: IPDatePickerComponent, row: Int, suggestedSymbol: String) -> String?
 
-    func datePicker(_ datePicker: IPDatePicker, rowHeightForComponent: IPDatePickerComponent) -> CGFloat?
-    func datePicker(_ datePicker: IPDatePicker, widthForComponent: IPDatePickerComponent) -> CGFloat?
+    func datePicker(_ datePicker: IPDatePicker, rowHeightForComponent component: IPDatePickerComponent) -> CGFloat?
+    func datePicker(_ datePicker: IPDatePicker, widthForComponent component: IPDatePickerComponent) -> CGFloat?
 
     func datePicker(_ datePicker: IPDatePicker, didSelectDate date: Date)
     func datePicker(_ datePicker: IPDatePicker, didSelectRow row: Int, inComponent: IPDatePickerComponent)
 }
 
-extension IPDatePickerDelegate {
-    func datePicker(_ datePicker: IPDatePicker, viewForItemForComponent: IPDatePickerComponent, row: Int, suggestedSymbol: String, reusing view: UIView?) -> UIView? {
+public extension IPDatePickerDelegate {
+    func datePicker(_ datePicker: IPDatePicker, viewForItemForComponent component: IPDatePickerComponent, row: Int, suggestedSymbol: String, reusing view: UIView?) -> UIView? {
         return nil
     }
 
-    func datePicker(_ datePicker: IPDatePicker, attributedSymbolForComponent: IPDatePickerComponent, row: Int, suggestedSymbol: String) -> NSAttributedString? {
+    func datePicker(_ datePicker: IPDatePicker, attributedSymbolForComponent component: IPDatePickerComponent, row: Int, suggestedSymbol: String) -> NSAttributedString? {
         return nil
     }
 
-    func datePicker(_ datePicker: IPDatePicker, symbolForComponent: IPDatePickerComponent, row: Int, suggestedSymbol: String) -> String? {
+    func datePicker(_ datePicker: IPDatePicker, symbolForComponent component: IPDatePickerComponent, row: Int, suggestedSymbol: String) -> String? {
         return nil
     }
 
-    func datePicker(_ datePicker: IPDatePicker, rowHeightForComponent: IPDatePickerComponent) -> CGFloat? {
+    func datePicker(_ datePicker: IPDatePicker, rowHeightForComponent component: IPDatePickerComponent) -> CGFloat? {
         return nil
     }
 
-    func datePicker(_ datePicker: IPDatePicker, widthForComponent: IPDatePickerComponent) -> CGFloat? {
+    func datePicker(_ datePicker: IPDatePicker, widthForComponent component: IPDatePickerComponent) -> CGFloat? {
         return nil
     }
 
