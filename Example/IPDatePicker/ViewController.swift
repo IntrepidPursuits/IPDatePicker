21279f00a35e45867e5834e51ebc66e4a8ecf408
Andrew Dolce <andrew@intrepid.io>
2017-06-20 10:26:02 -0400

    Add localization demonstration to example view

--- Example/IPDatePicker/ViewController.swift
+++ Example/IPDatePicker/ViewController.swift
@@ -11,15 +11,55 @@ import IPDatePicker
 import PureLayout
 
 class ViewController: UIViewController, IPDatePickerDelegate {
-    let picker = IPDatePicker()
+    private var picker = IPDatePicker()
+
+    let englishButton = UIButton(type: .system)
+    let simplifiedChineseButton = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupPickerWithLocale()
+        setupButtons()
+    }
+
+    private func setupPickerWithLocale(_ locale: Locale = .current) {
+        let date = picker.date
+        picker.view.removeFromSuperview()
+
+        picker = IPDatePicker(date: date, locale: locale)
+        picker.delegate = self
+
         view.addSubview(picker.view)
         picker.view.autoCenterInSuperview()
+    }
 
-        picker.delegate = self
+    private func setupButtons() {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+
+        stack.addArrangedSubview(englishButton)
+        stack.addArrangedSubview(simplifiedChineseButton)
+
+        englishButton.setTitle("English", for: .normal)
+        englishButton.addTarget(self, action: #selector(didPressEnglish), for: .touchUpInside)
+
+        simplifiedChineseButton.setTitle("Simplified Chinese", for: .normal)
+        simplifiedChineseButton.addTarget(self, action: #selector(didPressSimplifiedChinese), for: .touchUpInside)
+
+        view.addSubview(stack)
+        stack.autoPinEdgesToSuperviewEdges(with: UIEdgeInsetsMake(0.0, 40.0, 40.0, 40.0), excludingEdge: .top)
+    }
+
+    private dynamic func didPressEnglish() {
+        let locale = Locale(identifier: "en")
+        setupPickerWithLocale(locale)
+    }
+
+    private dynamic func didPressSimplifiedChinese() {
+        let locale = Locale(identifier: "zh")
+        setupPickerWithLocale(locale)
     }
 
     // MARK: - IPDatePickerDelegate
